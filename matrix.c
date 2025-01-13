5d7437308cd65cac4b1a3c1bfe61f4d13846c291
Venky Venkatakrishnan <venkyul@gmail.com>
2025-01-12 23:46:46 -0600

    Add files via upload

--- matrix.c
+++ matrix.c
@@ -4,8 +4,24 @@
 #include<time.h>
 #include<unistd.h>
 
-void update_state(char** state);
+#define KEY_q 81
+
+
+typedef struct _streak {
+        
+        int pos;
+        int length;
+        bool isActive;
+
+} streak;
+
+void update_state(char** state, streak* streaks);
 void rotate_right(char **arr, int n, int k);
+
+#define MAX_STREAK_LENGTH 10
+#define MAX_STREAKS 25
+
+#define ROW_LENGTH COLS - 2
 
 int main(int argc, char* argv[]) {
 
@@ -16,16 +32,33 @@ int main(int argc, char* argv[]) {
 
 	initscr();
 	cbreak();
-	mvprintw(1, (COLS - strlen(my_name))/2, my_name);
+    noecho();
+	mvprintw(1, (COLS - strlen(my_name))/2,"%s", my_name);
 	refresh();
 	curs_set(0);
-	timeout(500);
+	timeout(150);
 	keypad(stdscr, TRUE);
 
-//	start_color();
-//	init_pair(1, COLOR_WHITE, COLOR_CYAN);
+        streak streaks[MAX_STREAKS];
 
-	WINDOW* matrix = subwin(stdscr, 20, COLS - 2, 2, 2);
+        for(int s=0; s < MAX_STREAKS; s++)
+        {
+                streaks[s].isActive = rand()% 2 ? true: false;
+                
+                if (streaks[s].isActive == true) {
+
+                        streaks[s].pos = rand() % ROW_LENGTH;
+                } else {
+                        streaks[s].pos = 0;
+                }
+
+                streaks[s].length = 0;
+        }
+
+    start_color();
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+
+	WINDOW* matrix = subwin(stdscr, 20, ROW_LENGTH, 2, 2);
 	wbkgd(matrix, COLOR_PAIR(1));
 	wrefresh(matrix);
 	keypad(matrix, TRUE);
@@ -35,46 +68,34 @@ int main(int argc, char* argv[]) {
 	state = (char**)calloc(20,sizeof(char*));
 	for(int i=0; i< COLS; i++)
 	{
-		state[i] = (char*)calloc(COLS - 2, sizeof(char));
+		state[i] = (char*)calloc(ROW_LENGTH, sizeof(char));
 	}
 
 
 	for(int i=0; i < 20; i++) {
-		for(int j=0; j < COLS - 2; j++) 
+		for(int j=0; j < ROW_LENGTH; j++) 
 		{
 			state[i][j]=' ';
 		}
 	}
 
-
-	for(int j=0; j < COLS - 2; j++)
-	{
-		state[0][j] = (rand() % 2) == 1 ? '*': ' ';
-		state[2][j] = (rand() % 2) == 1 ? '*': ' ';
-	}
-
-	while(getch() != KEY_F(1)) 
+	while((ch = getch()) != 'q') 
 	{
 
-		update_state(state);
+		update_state(state, streaks);
 		wclear(matrix);
 		for (int i=0; i < 20; i++)
 		{
-			for(int j=0; j < COLS - 2; j++)
+			for(int j=0; j < ROW_LENGTH; j++)
 			{
 				mvwprintw(matrix, i + 1, j + 1,"%c",state[i][j]);
 			}
 		}
 			
 		wrefresh(matrix);
-	//	usleep(2000000);
-	//	sleep(1000);
-	//	timeout(1000);
-		
 
 	
 	}
-//	ch = getch();
 
 	endwin();
 
@@ -82,10 +103,57 @@ int main(int argc, char* argv[]) {
 
 }
 
-void update_state(char **state)
+char* get_streaks_row(streak* streaks)
 {
 
+        char *row = (char*)calloc(ROW_LENGTH, sizeof(char));
+        int r = 0;
+
+        while(r < 5) {
+                int rand_streak = rand()% MAX_STREAKS;
+
+                if (streaks[rand_streak].isActive == false) 
+                {
+                        streaks[rand_streak].isActive = true;
+                        streaks[rand_streak].pos = rand()%ROW_LENGTH;
+                }
+                r++;
+        }
+
+
+        for(int i=0; i< ROW_LENGTH; i++)
+        {
+                row[i]=' ';
+        }
+
+        for(int i=0; i < MAX_STREAKS; i++)
+        {
+                if(streaks[i].isActive == true)
+                {
+                        if (streaks[i].length < MAX_STREAK_LENGTH) 
+                        {
+                                char ch = (char) (rand()%127 + 32);
+                                row[streaks[i].pos] = ch;
+                                streaks[i].length++;
+                        } else {
+
+                                streaks[i].isActive = false;
+                                streaks[i].length = 0;
+                                streaks[i].pos = 0;
+                        }
+                }
+        }
+
+        return row;
+
+}
+void update_state(char **state, streak* streaks)
+{
+
+    char *row = get_streaks_row(streaks);
+
 	rotate_right(state, 20, 1);
+    state[0] = row;
 
 }
 
